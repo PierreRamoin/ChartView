bee565d2d2f3b1e189fb406d0911d1d2541410e3
Pierre Ramoin <pierre@ramoin.com>
2022-01-26 22:38:39 -0500

    First implementation of label for PieChart

--- Sources/SwiftUICharts/PieChart/PieChartCell.swift
+++ Sources/SwiftUICharts/PieChart/PieChartCell.swift
@@ -19,9 +19,13 @@ struct PieSlice: Identifiable {
 public struct PieChartCell: View {
     @State private var show: Bool = false
     @State var label: String
+    @State var showLabel = false
     var rect: CGRect
     var radius: CGFloat {
-        min(rect.width, rect.height) / 2
+        min(rect.width, rect.height) / (showLabel ? 3 : 2)
+    }
+    var bisector: Angle {
+        Angle(degrees: (self.startDeg + self.endDeg) / 2)
     }
     var startDeg: Double
     var endDeg: Double
@@ -30,6 +34,12 @@ public struct PieChartCell: View {
         path.addArc(center: rect.mid, radius: self.radius, startAngle: Angle(degrees: self.startDeg), endAngle: Angle(degrees: self.endDeg), clockwise: false)
         path.addLine(to: rect.mid)
         path.closeSubpath()
+        return path
+    }
+    var labelLine: Path {
+        var path = Path()
+        path.move(to: getPointOnBisector(distanceFromCenter: radius))
+        path.addLine(to: getPointOnBisector(distanceFromCenter: 1.3 * radius))
         return path
     }
     var index: Int
@@ -46,6 +56,24 @@ public struct PieChartCell: View {
                 .onAppear() {
                     self.show = true
                 }
+        if showLabel {
+            labelLine
+                .foregroundColor(.clear)
+                .overlay(labelLine.stroke(.white, lineWidth: 1))
+
+                .scaleEffect(show ? 1 : 0)
+                .animation(Animation.spring().delay(Double(self.index) * 0.04))
+                .onAppear() {
+                    self.show = true
+                }
+            Text(label)
+                    .position(getPointOnBisector(distanceFromCenter: 1.4 * radius))
+        }
+    }
+
+    private func getPointOnBisector(distanceFromCenter: Double) -> CGPoint {
+        CGPoint(x: Double(distanceFromCenter) * cos(bisector.radians) + rect.mid.x,
+                y: Double(distanceFromCenter) * sin(bisector.radians) + rect.mid.y)
     }
 }
 
@@ -60,7 +88,7 @@ struct PieChartCell_Previews: PreviewProvider {
     static var previews: some View {
         GeometryReader { geometry in
             PieChartCell(label: "toto", rect: geometry.frame(in: .local), startDeg: 0.0, endDeg: 90.0, index: 0, backgroundColor: Color(red: 252.0 / 255.0, green: 236.0 / 255.0, blue: 234.0 / 255.0), color: Color(red: 225.0 / 255.0, green: 97.0 / 255.0, blue: 76.0 / 255.0))
-        }.frame(width: 100, height: 100)
+        }
 
     }
 }
